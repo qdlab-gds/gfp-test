abeffec00a0cf8120c027f1c45dd15bc1845a680
flaport <floris.laporte@gmail.com>
2024-07-10 16:32:41 +0200

    improvements to simulate.py

--- simulate.py
+++ simulate.py
@@ -5,31 +5,30 @@ import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import sax
 from cspdk.si220 import PDK
+from dosax.circuit import circuit_plot
 
 PDK.activate()
 
 PICS_PATH = os.path.join("/", os.environ.get("GIT_REPO", ""), "pics")
 PDK.register_cells_yaml(PICS_PATH, update=True)
 
-c = gf.get_component("lattice")
-netlist = sax.netlist(c.get_netlist(recursive=True))
-print(
-    {
-        k: {k: c.component for k, c in v.instances.items()}
-        for k, v in netlist.root.items()
-    }
-)
-
-circuit, _ = sax.circuit(
-    netlist,
-    models=PDK.models,
-)
-
 wl = jnp.linspace(1.5, 1.6, 1000)
+c = gf.get_component("lattice")
+netlist = sax.netlist(c.get_netlist(recursive=True), with_placements=False)
+circuit = circuit_plot(
+    netlist,
+    pdk="cspdk.si220",
+    op="dB",
+    which="html",
+    port_in="o1",
+    host="https://dosax.docode.doplaydo.com",
+)
+with open("plot.html", "w") as file:
+    file.write(circuit(wl=wl))
+
+circuit, _ = sax.circuit(netlist, models=PDK.models)
 result = sax.sdict(circuit(wl=wl))
-print(list(result))
-
-
+plt.plot(wl, 10 * jnp.log10(abs(result["o1", "o1"]) ** 2))
 plt.plot(wl, 10 * jnp.log10(abs(result["o1", "o2"]) ** 2))
 plt.plot(wl, 10 * jnp.log10(abs(result["o1", "o3"]) ** 2))
 plt.show()
