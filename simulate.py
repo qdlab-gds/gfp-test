672af3412c1b9fc29a20df6cd3113791ce8560ca
flaport <floris.laporte@gmail.com>
2024-07-11 19:21:25 +0200

    show plots in vscode panel

--- simulate.py
+++ simulate.py
@@ -2,10 +2,12 @@ import os
 
 import gdsfactory as gf
 import jax.numpy as jnp
-import matplotlib.pyplot as plt
 import sax
 from cspdk.si220 import PDK
-from dosax.circuit import circuit_plot
+from dodesign.simulate import circuit_plot
+from dodesign.show import show
+import pandas as pd
+import altair as alt
 
 PDK.activate()
 
@@ -28,7 +30,39 @@ with open("plot.html", "w") as file:
 
 circuit, _ = sax.circuit(netlist, models=PDK.models)
 result = sax.sdict(circuit(wl=wl))
-plt.plot(wl, 10 * jnp.log10(abs(result["o1", "o1"]) ** 2))
-plt.plot(wl, 10 * jnp.log10(abs(result["o1", "o2"]) ** 2))
-plt.plot(wl, 10 * jnp.log10(abs(result["o1", "o3"]) ** 2))
-plt.show()
+
+# plt.plot(wl, 10 * jnp.log10(abs(result["o1", "o1"]) ** 2))
+# plt.plot(wl, 10 * jnp.log10(abs(result["o1", "o2"]) ** 2))
+# plt.plot(wl, 10 * jnp.log10(abs(result["o1", "o3"]) ** 2))
+# show()
+
+# from bokeh.plotting import figure
+# p = figure(title="Bokeh Plot", x_axis_label='Wavelength (um)', y_axis_label='dB')
+# p.line(wl, 10 * jnp.log10(abs(result["o1", "o1"]) ** 2), color="red")
+# p.line(wl, 10 * jnp.log10(abs(result["o1", "o2"]) ** 2), color="green")
+# p.line(wl, 10 * jnp.log10(abs(result["o1", "o3"]) ** 2), color="blue")
+# show(p)
+
+
+dfs: list[pd.DataFrame] = []
+for (p, q), arr in result.items():
+    if p == "o1" and q != "o1":
+        dB = 20 * jnp.log10(abs(arr))
+        dfs.append(pd.DataFrame({"wl": wl, "dB": dB, "port_out": q}))
+df = pd.concat(dfs, axis=0)
+sel = alt.selection_point(fields=["port_out"], bind="legend", toggle="true", empty=True)
+c = (
+    alt.Chart(df)
+    .mark_line()
+    .encode(
+        x=alt.X("wl"),
+        y="dB",
+        color="port_out",
+        opacity=alt.condition(sel, alt.value(0.9), alt.value(0.1)),  # type: ignore
+    )
+    .add_params(sel)
+    .properties(width="container", height=250)
+    .interactive()
+)
+
+show(c)
